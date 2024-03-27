a9f9bdd680b381672e19a855d6b564c25d1f965f
wjddidgjs <164023898+wjddidgjs@users.noreply.github.com>
2024-03-27 20:37:14 +0900

    Create 2753.c

---
+++ week02/2753.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	scanf("%d", &a);
+
+	if (a % 4 == 0)
+	{
+		if (a % 100 != 0)
+			printf("1");
+		else if (a % 400 == 0)
+			printf("1");
+		else
+			printf("0");
+	}
+	else
+		printf("0");
+}
