9552a1b60c3641009553ec397c65676fd79f724a
wjddidgjs <164023898+wjddidgjs@users.noreply.github.com>
2024-03-30 17:48:49 +0900

    Create 2480.c

---
+++ week02/2480.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	int A, B, C;
+	int D = 0;
+	scanf_s("%d %d %d", &A, &B, &C);
+	if (A == B && B == C && C == A)
+		D = 10000 + A * 1000;
+	else if (A == B || B == C || C == A)
+		D = 1000 + A * 100;
+	else
+	{
+	if (A > B && A > C)
+		D = A * 100;
+	else if (B > A && B > C)
+		D = B * 100;
+	else if (C > A && C > B)
+		D = C * 100;
+	}
+	printf("%d", D);
+}
